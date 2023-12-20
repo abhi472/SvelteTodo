3dd04f5886ec8e3b8639e9243fa589a854db98bb
joltedThought <95027707+joltedThought@users.noreply.github.com>
2023-12-20 08:19:00 +0530

    added routes

---
+++ src/routes/logic/+page.svelte
@@ -0,0 +1,35 @@
+<div id="app"></div>
+
+<script>
+	import {onMount} from 'svelte'
+	onMount(() => {
+  let appElement = document.querySelector('#app')
+
+  let user = {
+	  loggedIn: false
+	}
+
+	function toggle() {
+	  user.loggedIn = !user.loggedIn
+		updateUI()
+	}
+
+	function updateUI() {
+    let html
+
+	  if (user.loggedIn) {
+		  html = `<button>Log out</button>`
+		}
+
+		if (!user.loggedIn) {
+		  html = `<button>Log in</button>`
+		}
+
+	  appElement.innerHTML = html
+    appElement.querySelector('button').onclick = toggle
+	}
+	
+
+  updateUI()
+		})
+</script>
---
+++ src/routes/reactive/+page.svelte
@@ -0,0 +1,46 @@
+  
+    <p></p>
+    <button on:click={addTrack}>click</button>
+    
+    <p>
+        Album length is {albumLength.minutes} minutes and {albumLength.seconds} seconds
+    </p>
+<script>
+    let album = [
+        {track: 'Track 1', length: 100},
+        {track: 'Track 2', length: 200},
+        {track: 'Track 3', length: 300},
+    ]
+    
+    /**
+     *  $ sign tells that the variable is reactive, 
+     * i.e. it observes changes to its source and emits the result
+     */
+    $: albumLength = getAlbumLength(album);
+    
+    function getAlbumLength(album) {
+        let lengthSeconds = album.reduce (
+            (a,b) => {
+                return a+b.length;
+            }, 0);
+    
+            let [minutes, seconds] = (lengthSeconds/60)
+            .toFixed(2)
+            .toString()
+            .split('.');
+            return {minutes,seconds}
+        
+    }
+     
+    function addTrack() {
+    
+        album = [...album, {track: 'Track 4', length: 200}];
+    
+    }
+    
+    
+    
+    
+    </script>
+  
+    
